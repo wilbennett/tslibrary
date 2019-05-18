3c9479fc31f7c86a045518222525377ba93436aa
Wil Bennett <wilbennettjr@live.com>
2019-05-18 11:53:06 -0400

    Added array extensions.

---
+++ src/utils/array-extensions.ts
@@ -0,0 +1,18 @@
+export { };
+
+declare global {
+    interface Array<T> {
+        remove(item: T): boolean;
+    }
+}
+
+if (!Array.prototype.remove) {
+    Array.prototype.remove = function (item: any) {
+        let index = this.indexOf(item);
+
+        if (index < 0) return false;
+
+        this.splice(index, 1);
+        return true;
+    }
+}
