55b06ef34155289116fb49b537d734c5b40feab3
Wil Bennett <wilbennettjr@live.com>
2019-06-05 18:56:45 -0400

    Refactored buffer and values creation.

--- src/vectors/vectorF32-groups-builder.ts
+++ src/vectors/vectorF32-groups-builder.ts
@@ -11,8 +11,8 @@ export class VectorF32GroupsBuilder extends VectorGroupsBuilder {
     protected _groups = new VectorBGroups();
 
     get Groups(): VectorGroups {
-        const buffer = new ArrayBuffer(this._groups.elementCount * Float32Array.BYTES_PER_ELEMENT);
-        const array = new Float32Array(buffer);
+        const buffer = this.createBuffer(this._groups.elementCount);
+        const array = this.createValues(buffer);
         let index = 0;
         let collections = this._groups.groups.values();
 
@@ -41,4 +41,10 @@ export class VectorF32GroupsBuilder extends VectorGroupsBuilder {
 
         return this;
     }
+
+    protected createBuffer(elementCount: number) {
+        return new ArrayBuffer(elementCount * Float32Array.BYTES_PER_ELEMENT);
+    }
+
+    protected createValues(buffer: ArrayBuffer) { return new Float32Array(buffer); }
 }
