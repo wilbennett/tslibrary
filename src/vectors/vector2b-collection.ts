721e51e8c5e12e83e7db40fc14667afc94a889cf
Wil Bennett <wilbennettjr@live.com>
2019-06-05 18:41:31 -0400

    Inherit from VectorBCollection.

--- src/vectors/vector2b-collection.ts
+++ src/vectors/vector2b-collection.ts
@@ -1,42 +1,15 @@
-import { Vector, Vector2B, Vector2BIndexer, VectorCollection, VectorData } from '.';
+import { Vector2B, Vector2BIndexer, VectorBCollection, VectorData } from '.';
 
-export class Vector2BCollection extends VectorCollection {
-    protected static readonly ERROR_NO_VALUES = "setStorage must be called before accessing values.";
-
+export class Vector2BCollection extends VectorBCollection {
     constructor(count: number) {
-        super();
-
-        this._length = count;
+        super(count);
     }
 
-    protected _values?: VectorData;
-    get values() {
-        if (!this._values)
-            throw Vector2BCollection.ERROR_NO_VALUES;
-
-        return this._values;
-    }
-    protected _startIndex: number = 0;
-    get startIndex() { return this._startIndex; }
-
-    protected _length: number;
-    get length() { return this._length; }
     get elementCount() { return this.length * Vector2B.elementCount; };
-    private _items?: Vector[];
-    get items() { return this._items || (this._items = this.createItems()); }
 
     createIndexer() { return new Vector2BIndexer(this); }
 
-    setStorage(values: VectorData, startIndex: number) {
-        this._values = values;
-        this._startIndex = startIndex;
-        this._items = undefined;
-    }
-
-    protected createItems() {
-        if (!this._values)
-            throw Vector2BCollection.ERROR_NO_VALUES;
-
+    protected createItemsCore(values: VectorData) {
         const length = this._length;
         let index = this._startIndex;
 
@@ -44,7 +17,7 @@ export class Vector2BCollection extends VectorCollection {
             this._items = new Array<Vector2B>(length);
 
         for (let i = 0; i < length; i++) {
-            this._items[i] = new Vector2B(this._values, index);
+            this._items[i] = new Vector2B(values, index);
             index += Vector2B.elementCount;
         }
 
