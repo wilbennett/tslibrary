756a1ee8647d0dbbac165526b92ff5e9d28b27b7
Wil Bennett <wilbennettjr@live.com>
2019-06-05 19:14:07 -0400

    Renamed _valuesF32 to _values.

--- src/vectors/vector2b-indexer.ts
+++ src/vectors/vector2b-indexer.ts
@@ -1,22 +1,22 @@
 import { Vector, Vector2B, Vector2BCollection, VectorData, VectorIndexer } from '.';
 
 export class Vector2BIndexer extends VectorIndexer {
-    protected _valuesF32: VectorData;
+    protected _values: VectorData;
     protected _startIndex: number;
 
     constructor(collection: Vector2BCollection) {
         super(collection);
-        this._valuesF32 = collection.values;
+        this._values = collection.values;
         this._startIndex = collection.startIndex;
     }
 
     get current() { return this._collection.get(this._index); }
-    get x() { return this._valuesF32[this._index * Vector2B.elementCount + this._startIndex]; }
-    set x(value) { this._valuesF32[this._index * Vector2B.elementCount + this._startIndex] = value; }
-    get y() { return this._valuesF32[this._index * Vector2B.elementCount + this._startIndex + 1]; }
-    set y(value) { this._valuesF32[this._index * Vector2B.elementCount + this._startIndex + 1] = value; }
-    get w() { return this._valuesF32[this._index * Vector2B.elementCount + this._startIndex + 2]; }
-    set w(value) { this._valuesF32[this._index * Vector2B.elementCount + this._startIndex + 2] = value; }
+    get x() { return this._values[this._index * Vector2B.elementCount + this._startIndex]; }
+    set x(value) { this._values[this._index * Vector2B.elementCount + this._startIndex] = value; }
+    get y() { return this._values[this._index * Vector2B.elementCount + this._startIndex + 1]; }
+    set y(value) { this._values[this._index * Vector2B.elementCount + this._startIndex + 1] = value; }
+    get w() { return this._values[this._index * Vector2B.elementCount + this._startIndex + 2]; }
+    set w(value) { this._values[this._index * Vector2B.elementCount + this._startIndex + 2] = value; }
 
     protected updateCurrent(index: number) {
         this._index = index;
