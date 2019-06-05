d5c9d7073c9b2b11a4403ed2a7ce2632118efa2e
Wil Bennett <wilbennettjr@live.com>
2019-06-05 18:08:31 -0400

    Refactored buffer size calculation.

--- src/vectors/vectorF32-groups-builder.ts
+++ src/vectors/vectorF32-groups-builder.ts
@@ -4,8 +4,7 @@ export class VectorF32GroupsBuilder extends VectorGroupsBuilder {
     protected _groups = new VectorBGroups();
 
     get Groups(): VectorGroups {
-        const int32bytes = 32 / 8;
-        const buffer = new ArrayBuffer(this._groups.elementCount * int32bytes);
+        const buffer = new ArrayBuffer(this._groups.elementCount * Float32Array.BYTES_PER_ELEMENT);
         const array = new Float32Array(buffer);
         let index = 0;
         let collections = this._groups.groups.values();
