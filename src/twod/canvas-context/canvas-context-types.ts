89ea4edc571ce326cfc409c47285801684db0e86
Wil Bennett <wilbennettjr@live.com>
2019-06-26 17:39:59 -0400

    Removed Composition enum.

--- src/twod/canvas-context/canvas-context-types.ts
+++ src/twod/canvas-context/canvas-context-types.ts
@@ -4,35 +4,6 @@ import { Color } from '../../colors';
 export type CanvasColor = string | Color;
 export type Brush = CanvasColor | CanvasGradient | CanvasPattern;
 
-export const enum Composition {
-    sourceOver = "source-over",
-    sourceIn = "source-in",
-    sourceOut = "source-out",
-    sourceAtop = "source-atop",
-    destinationOver = "destination-over",
-    destinationIn = "destination-in",
-    destinationOut = "destination-out",
-    destinationAtop = "destination-atop",
-    lighter = "lighter",
-    copy = "copy",
-    xor = "xor",
-    multiply = "multiply",
-    screen = "screen",
-    overlay = "overlay",
-    darken = "darken",
-    lighten = "lighten",
-    colorDodge = "color-dodge",
-    colorBurn = "color-burn",
-    hardLight = "hard-light",
-    softLight = "soft-light",
-    difference = "difference",
-    exclusion = "exclusion",
-    hue = "hue",
-    saturation = "saturation",
-    color = "color",
-    luminosity = "luminosity"
-}
-
 export type Compositions =
     | "source-over"
     | "source-in"
