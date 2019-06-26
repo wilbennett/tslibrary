c9e68d2f7427216511b7c25e570b264065e4d40f
Wil Bennett <wilbennettjr@live.com>
2019-06-26 16:44:41 -0400

    Renamed to Composition.

--- src/twod/canvas-context/canvas-context-types.ts
+++ src/twod/canvas-context/canvas-context-types.ts
@@ -4,7 +4,7 @@ import { Color } from '../../colors';
 export type CanvasColor = string | Color;
 export type Brush = CanvasColor | CanvasGradient | CanvasPattern;
 
-export const enum CompositionOperation {
+export const enum Composition {
     sourceOver = "source-over",
     sourceIn = "source-in",
     sourceOut = "source-out",
@@ -32,3 +32,31 @@ export const enum CompositionOperation {
     color = "color",
     luminosity = "luminosity"
 }
+
+export type Compositions =
+    | "source-over"
+    | "source-in"
+    | "source-out"
+    | "source-atop"
+    | "destination-over"
+    | "destination-in"
+    | "destination-out"
+    | "destination-atop"
+    | "lighter"
+    | "copy"
+    | "xor"
+    | "multiply"
+    | "screen"
+    | "overlay"
+    | "darken"
+    | "lighten"
+    | "color-dodge"
+    | "color-burn"
+    | "hard-light"
+    | "soft-light"
+    | "difference"
+    | "exclusion"
+    | "hue"
+    | "saturation"
+    | "color"
+    | "luminosity";
