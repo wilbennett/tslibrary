d549f1b25dd1f554c1ef500b3840bc302a2023f5
Wil Bennett <wilbennettjr@live.com>
2019-06-29 20:03:27 -0400

    Changed to be mutable. Refactored to only store center and half size.

--- src/misc/bounds.ts
+++ src/misc/bounds.ts
@@ -3,15 +3,6 @@ import { Vector, Vector2D, Vector3D } from '../vectors';
 export type BoundsDirection = "up" | "down";
 
 export class Bounds {
-    private static _zero: Bounds;
-    private _halfSize?: Vector;
-    private _center?: Vector;
-    private _max?: Vector;
-    protected _topLeft?: Vector;
-    protected _bottomLeft?: Vector;
-    protected _bottomRight?: Vector;
-    protected _topRight?: Vector;
-
     constructor(position: Vector, size: Vector, direction?: BoundsDirection);
     constructor(x: number, y: number, z: number, width: number, height: number, depth: number, direction?: BoundsDirection);
     constructor(x: number, y: number, width: number, height: number, direction?: BoundsDirection);
@@ -22,56 +13,57 @@ export class Bounds {
     constructor(mode: "centerhalf", centerX: number, centerY: number, centerZ: number, halfWidth: number, halfHeight: number, halfDepth: number, direction?: BoundsDirection);
     constructor(mode: "centerhalf", centerX: number, centerY: number, halfWidth: number, halfHeight: number, direction?: BoundsDirection);
     constructor(param1: Vector | number | string, param2: any, param3?: any, param4?: any, param5?: any, param6?: any, param7?: any, param8?: any) {
-        this.position = Vector.empty;
-        this.size = Vector.empty;
+        this.center = Vector.empty;
+        this.halfSize = Vector.empty;
         this.direction = "up";
 
-        if (param1 instanceof Vector) {
-            this.position = param1;
-            this.size = param2;
+        if (param1 instanceof Vector && param2 instanceof Vector) {
+            const position = param1;
+            this.halfSize = param2.scaleN(0.5);
+            this.center = position.displaceByN(this.halfSize);
             this.direction = param3 || "up";
         } else if (typeof param1 === "number") {
             if (arguments.length >= 6) {
-                this.position = Vector3D.createPosition(param1, param2, param3);
-                this.size = Vector3D.createDirection(param4, param5, param6);
+                const position = Vector3D.createPosition(param1, param2, param3);
+                this.halfSize = Vector3D.createDirection(param4 * 0.5, param5 * 0.5, param6 * 0.5);
+                this.center = position.displaceByN(this.halfSize);
                 this.direction = param7 || "up";
             } else {
-                this.position = Vector2D.createPosition(param1, param2);
-                this.size = Vector2D.createDirection(param3, param4);
+                const position = Vector2D.createPosition(param1, param2);
+                this.halfSize = Vector2D.createDirection(param3 * 0.5, param4 * 0.5);
+                this.center = position.displaceByN(this.halfSize);
                 this.direction = param5 || "up";
             }
         } else if (typeof param1 === "string") { // center or centerhalf.
             if (param1 === "center") {
-                if (param2 instanceof Vector) {
-                    this.size = param3;
-                    this.position = param2.displaceByN(this.size.scaleN(0.5).negate());
+                if (param2 instanceof Vector && param3 instanceof Vector) {
+                    this.halfSize = param3.scaleN(0.5);
+                    this.center = param2;
                     this.direction = param4 || "up";
                 } else if (typeof param2 === "number") {
                     if (arguments.length >= 7) {
-                        this.size = Vector3D.createDirection(param5, param6, param7);
+                        this.halfSize = Vector3D.createDirection(param5 * 0.5, param6 * 0.5, param7 * 0.5);
                         this.direction = param8 || "up";
-                        const half = this.size.scaleN(0.5);
-                        this.position = Vector3D.createPosition(param2 - half.x, param3 - half.y, param4 - half.z);
+                        this.center = Vector3D.createPosition(param2, param3, param4);
                     } else {
-                        this.size = Vector2D.createDirection(param4, param5);
+                        this.halfSize = Vector2D.createDirection(param4 * 0.5, param5 * 0.5);
                         this.direction = param6 || "up";
-                        const half = this.size.scaleN(0.5);
-                        this.position = Vector2D.createPosition(param2 - half.x, param3 - half.y);
+                        this.center = Vector2D.createPosition(param2, param3);
                     }
                 }
             } else { // centerhalf.
                 if (param2 instanceof Vector && param3 instanceof Vector) {
-                    this.size = param3.scaleN(2);
-                    this.position = param2.displaceByN(param3.negateN());
+                    this.center = param2;
+                    this.halfSize = param3;
                     this.direction = param4 || "up";
                 } else if (typeof param2 === "number") {
                     if (arguments.length >= 7) {
-                        this.position = Vector3D.createPosition(param2 - param5, param3 - param6, param4 - param7);
-                        this.size = Vector3D.createDirection(param5 * 2, param6 * 2, param7 * 2);
+                        this.center = Vector3D.createPosition(param2, param3, param4);
+                        this.halfSize = Vector3D.createDirection(param5, param6, param7);
                         this.direction = param8 || "up";
                     } else {
-                        this.position = Vector2D.createPosition(param2 - param4, param3 - param5);
-                        this.size = Vector2D.createDirection(param4 * 2, param5 * 2);
+                        this.center = Vector2D.createPosition(param2, param3);
+                        this.halfSize = Vector2D.createDirection(param4, param5);
                         this.direction = param6 || "up";
                     }
                 }
@@ -79,8 +71,8 @@ export class Bounds {
         }
     }
 
-    public readonly position: Vector;
-    public readonly size: Vector;
+    public center: Vector;
+    public halfSize: Vector;
     public readonly direction: BoundsDirection;
     get x() { return this.position.x; }
     get y() { return this.position.y; }
@@ -91,66 +83,42 @@ export class Bounds {
     get w() { return this.size.x; }
     get h() { return this.size.y; }
     get d() { return this.size.z; }
-    get halfSize() {
-        if (!this._halfSize)
-            this._halfSize = this.size.scaleN(0.5);
-
-        return this._halfSize;
-    }
-    get max() {
-        if (!this._max)
-            this._max = this.position.displaceByN(this.size);
-
-        return this._max;
-    }
+    get position() { return this.center.subN(this.halfSize); }
+    get size() { return this.halfSize.scaleN(2); }
+    get max() { return this.center.addN(this.halfSize); }
     get topLeft() {
-        if (!this._topLeft) {
-            this._topLeft = this.direction === "up"
-                ? this.position.displaceByN(this.size.withXYZN(0, this.size.y, 0))
-                : this.position;
-        }
+        const halfSize = this.halfSize;
 
-        return this._topLeft;
+        return this.direction === "up"
+            ? this.center.addN(new Vector3D(-halfSize.x, halfSize.y, -halfSize.z))
+            : this.center.addN(new Vector3D(-halfSize.x, -halfSize.y, -halfSize.z));
     }
     get bottomLeft() {
-        if (!this._bottomLeft) {
-            this._bottomLeft = this.direction === "up"
-                ? this.position
-                : this.position.displaceByN(this.size.withXYZN(0, this.size.y, 0));
-        }
+        const halfSize = this.halfSize;
 
-        return this._bottomLeft;
+        return this.direction === "up"
+            ? this.center.addN(new Vector3D(-halfSize.x, -halfSize.y, -halfSize.z))
+            : this.center.addN(new Vector3D(-halfSize.x, halfSize.y, -halfSize.z));
     }
     get topRight() {
-        if (!this._topRight) {
-            this._topRight = this.direction === "up"
-                ? this.position.displaceByN(this.size.withXYZN(this.size.x, this.size.y, 0))
-                : this.position.displaceByN(this.size.withXYZN(this.size.x, 0, 0));
-        }
+        const halfSize = this.halfSize;
 
-        return this._topRight;
+        return this.direction === "up"
+            ? this.center.addN(new Vector3D(halfSize.x, halfSize.y, -halfSize.z))
+            : this.center.addN(new Vector3D(halfSize.x, -halfSize.y, -halfSize.z));
     }
     get bottomRight() {
-        if (!this._bottomRight) {
-            this._bottomRight = this.direction === "up"
-                ? this.position.displaceByN(this.size.withXYZN(this.size.x, 0, 0))
-                : this.position.displaceByN(this.size.withXYZN(this.size.x, this.size.y, 0));
-        }
+        const halfSize = this.halfSize;
 
-        return this._bottomRight;
+        return this.direction === "up"
+            ? this.center.addN(new Vector3D(halfSize.x, -halfSize.y, -halfSize.z))
+            : this.center.addN(new Vector3D(halfSize.x, halfSize.y, -halfSize.z));
     }
-    get left() { return this.position.x; }
-    get right() { return this.topRight.x; }
+    get left() { return this.center.x - this.halfSize.x; }
+    get right() { return this.center.x + this.halfSize.x; }
 
-    get top() { return this.topLeft.y; }
-    get bottom() { return this.bottomLeft.y; }
-
-    get center() {
-        if (!this._center)
-            this._center = this.position.displaceByN(this.halfSize);
-
-        return this._center;
-    }
+    get top() { return this.direction === "up" ? this.center.y + this.halfSize.y : this.center.y - this.halfSize.y; }
+    get bottom() { return this.direction === "up" ? this.center.y - this.halfSize.y : this.center.y + this.halfSize.y; }
 
     get centerX() { return this.center.x; }
     get centerY() { return this.center.y; }
@@ -242,6 +210,8 @@ export class Bounds {
             && this.y <= otherMax.y && max.y >= other.y
             && this.z <= otherMax.z && max.z >= other.z;
     }
+
+    private static _zero: Bounds;
 
     static get zero() {
         if (!this._zero)
