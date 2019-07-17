8f2566329697899fdc283fcb3335e3ca6c012112
Wil Bennett <wilbennettjr@live.com>
2019-07-17 18:18:38 -0400

    Added array parameter overload.

--- src/colors/color-utils.ts
+++ src/colors/color-utils.ts
@@ -1,7 +1,11 @@
-import { Color, SColor } from '.';
+import { CanvasColor, Color, SColor } from '.';
 
 export class ColorUtils {
-    static toColorArray(...colors: (string | Color)[]): Color[] {
+    static toColorArray(colors: CanvasColor[]): Color[];
+    static toColorArray(...colors: CanvasColor[]): Color[];
+    static toColorArray(param1: any): Color[] {
+        let colors = <CanvasColor[]>(Array.isArray(param1) ? param1 : Array.from(arguments));
+
         return colors.map(c => c instanceof Color ? c : new SColor(c));
     }
 }
