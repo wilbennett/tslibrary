467dbf038274f0264fd248674b96880b20720031
Wil Bennett <wilbennettjr@live.com>
2019-11-05 16:58:29 -0500

    Added 2D utilities.

---
+++ src/twod/utils/utils2d.ts
@@ -0,0 +1,10 @@
+import { Vector } from '../../vectors';
+
+export function calcIntersectionTime(point: Vector, direction: Vector, otherPoint: Vector, otherDirection: Vector) {
+  const denom = otherDirection.cross2D(direction);
+
+  if (denom === 0) return null;
+
+  const c = otherPoint.subN(point);
+  return otherDirection.cross2D(c) / denom;
+}
