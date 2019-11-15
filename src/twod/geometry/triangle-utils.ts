b01724fec4c2e77562b68f663b3e4646c2cd370b
Wil Bennett <wilbennettjr@live.com>
2019-11-14 23:16:57 -0500

    Added non geometric functions.

--- src/twod/geometry/triangle-utils.ts
+++ src/twod/geometry/triangle-utils.ts
@@ -1,6 +1,11 @@
 import { ITriangle } from '.';
+import { Vector } from '../../vectors';
 
-export function signedTriangleArea(triangle: ITriangle) {
+export function signedTriangleArea(a: Vector, b: Vector, c: Vector) {
+  return (a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x);
+}
+
+export function signedGeoTriangleArea(triangle: ITriangle) {
   const vertices = triangle.vertices.items;
   const a = vertices[0];
   const b = vertices[1];
@@ -8,11 +13,22 @@ export function signedTriangleArea(triangle: ITriangle) {
   return (a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x);
 }
 
-export function isTriangleCCW(triangle: ITriangle) {
+export function isTriangleCCW(a: Vector, b: Vector, c: Vector) {
+  const signedArea = (a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x);
+  return signedArea >= 0;
+
+  // https://www.toptal.com/python/computational-geometry-in-python-from-theory-to-implementation
+  // return (b.x - a.x) * (c.y - a.y) > (b.y - a.y) * (c.x - a.x);
+}
+
+export function isGeoTriangleCCW(triangle: ITriangle) {
   const vertices = triangle.vertices.items;
   const a = vertices[0];
   const b = vertices[1];
   const c = vertices[2];
   const signedArea = (a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x);
   return signedArea >= 0;
+
+  // https://www.toptal.com/python/computational-geometry-in-python-from-theory-to-implementation
+  // return (b.x - a.x) * (c.y - a.y) > (b.y - a.y) * (c.x - a.x);
 }
