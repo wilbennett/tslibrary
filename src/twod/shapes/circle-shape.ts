3b39e0220a72c5ffef29eb869835e0fcde0ec5a2
Wil Bennett <wilbennettjr@live.com>
2019-11-18 14:18:31 -0500

    Fixed bug in getting dynamic axes.

--- src/twod/shapes/circle-shape.ts
+++ src/twod/shapes/circle-shape.ts
@@ -1,13 +1,10 @@
-import { ICircleShape, Shape, ShapeBase } from '.';
+import { ICircleShape, ORIGIN, Shape, ShapeBase } from '.';
 import { ContextProps, EulerSemiImplicit, Integrator, IntegratorConstructor, Viewport } from '..';
 import { Tristate } from '../../core';
-import { MatrixValues } from '../../matrix';
 import { Vector } from '../../vectors';
 
 export class CircleShape extends ShapeBase implements ICircleShape {
   kind: "circle" = "circle";
-  protected _localTransform: MatrixValues;
-  protected _localTransformInverse: MatrixValues;
 
   constructor(
     radius: number,
@@ -23,9 +20,6 @@ export class CircleShape extends ShapeBase implements ICircleShape {
     this._integratorType = integratorType;
     this._integrator = new integratorType();
     this._integrators = [this._integrator];
-
-    this._localTransform = this.matrix.createValues();
-    this._localTransformInverse = this.matrix.createValues();
   }
 
   protected _integratorType: IntegratorConstructor;
@@ -40,7 +34,6 @@ export class CircleShape extends ShapeBase implements ICircleShape {
   radius: number;
   get hasDynamicAxes() { return true; }
 
-  //*
   getDynamicAxes(other: Shape, result?: Vector[]): Vector[] {
     result || (result = []);
     const posInOtherSpace = other.toLocal(this.position);
@@ -48,13 +41,13 @@ export class CircleShape extends ShapeBase implements ICircleShape {
     // console.log(`position: ${this.position} => ${posInOtherSpace}, closest: ${closest}`);
 
     // If centers are at the same position, return an arbitrary axis.
-    if (closest.equals(other.position)) {
+    if (closest.equals(ORIGIN)) {
       result.push(Vector.createDirection(0, 1));
       return result;
     }
 
     // Center is inside other shape.
-    if (closest.equals(posInOtherSpace)) {
+    if (closest.equals(this.position)) {
       const axis = this.position.subO(other.position).normalize();
       result.push(axis);
       return result;
@@ -64,50 +57,6 @@ export class CircleShape extends ShapeBase implements ICircleShape {
     result.push(axis);
     return result;
   }
-  /*/
-  getDynamicAxes(other: Shape, result?: Vector[]): Vector[] {
-    result || (result = []);
-    const posInOtherSpace = other.toLocal(this.position);
-    const closest = other.toWorld(other.closestPoint(posInOtherSpace));
-
-    // If centers are at the same position, return an arbitrary axis.
-    if (closest.equals(other.position)) {
-      result.push(Vector.createDirection(0, 1));
-      return result;
-    }
-
-    // Center is inside other shape.
-    if (closest.equals(posInOtherSpace)) {
-      const axis = this.position.subO(other.position).normalize();
-      result.push(axis);
-      return result;
-    }
-
-    const axis = this.position.subO(closest).normalize();
-    result.push(axis);
-    return result;
-  }
-  //*/
-
-  /*
-  toWorld(localPoint: Vector, result?: Vector) {
-    if (this.isWorld) {
-      result = result || Vector.create(0, 0);
-      return result.copyFrom(localPoint);
-    }
-
-    return this.matrix.transform(localPoint, this._localTransform, result);
-  }
-
-  toLocal(worldPoint: Vector, result?: Vector) {
-    if (this.isWorld) {
-      result = result || Vector.create(0, 0);
-      return result.copyFrom(worldPoint);
-    }
-
-    return this.matrix.transform(worldPoint, this._localTransformInverse, result);
-  }
-  //*/
 
   protected getSupportFromVector(direction: Vector, result?: Vector): Tristate<number | Vector> {
     result || (result = Vector.create(0, 0));
@@ -125,21 +74,5 @@ export class CircleShape extends ShapeBase implements ICircleShape {
 
     props.fillStyle && ctx.fill();
     props.strokeStyle && ctx.stroke();
-  }
-
-  protected calcTransform(transform: MatrixValues, transformInverse: MatrixValues) {
-    const matrix = this.matrix;
-
-    matrix
-      .setToIdentity()
-      .setRotation2D(this.angle);
-
-    matrix.getValues(this._localTransform);
-    matrix.getInverse(this._localTransformInverse);
-
-    matrix.setTranslation(this.position);
-
-    matrix.getValues(transform);
-    matrix.getInverse(transformInverse);
   }
 }
