5a99edced176f53dd9c1d2fc8cc49a3e8bf3a91a
Wil Bennett <wilbennettjr@live.com>
2019-11-19 22:58:50 -0500

    Removed axes support.

--- src/twod/shapes/plane-shape.ts
+++ src/twod/shapes/plane-shape.ts
@@ -1,6 +1,5 @@
 import { IPlaneShape, ShapeBase } from '.';
 import { ContextProps, Viewport } from '..';
-import { Tristate } from '../../core';
 import { Vector } from '../../vectors';
 
 export class PlaneShape extends ShapeBase implements IPlaneShape {
@@ -40,35 +39,11 @@ export class PlaneShape extends ShapeBase implements IPlaneShape {
     this.dirtyTransform();
   }
 
-  getAxes(result?: Vector[]): Vector[] {
-    result || (result = []);
-    result.push(this.normal);
-    return result;
-  }
-
-  // projectOn(worldAxis: Vector, result?: Projection): Tristate<Projection> {
-  //   const value = this.position.dot(worldAxis);
-
-  //   result || (result = new Projection());
-
-  //   result.min = value;
-  //   result.max = value;
-  //   result.minPoint = this.position;
-  //   result.maxPoint = this.position;
-
-  //   return result;
-  // }
-
-  // @ts-ignore - unused param.
-  protected getSupportFromVector(direction: Vector, result?: Vector): Tristate<number | Vector> {
-    return result;
-  }
-
   protected renderCore(view: Viewport, props: ContextProps) {
     const mag = view.viewBounds.max.subO(view.viewBounds.min).magSquared;
     const dir = this.direction.scale(mag);
     const start = dir.asPosition();
-    const end = dir.negateO();
+    const end = dir.negateO().asPosition();
 
     view.ctx.beginPath().line(start, end).stroke();
     this.normal.render(view, Vector.createPosition(0, 0), props);
