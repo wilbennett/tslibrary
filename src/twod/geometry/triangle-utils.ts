8d67eaf7e025be0e8d6a7ee36a4baa809aee38b1
Wil Bennett <wilbennettjr@live.com>
2019-11-20 23:21:26 -0500

    Added check for clockwise winding.

--- src/twod/geometry/triangle-utils.ts
+++ src/twod/geometry/triangle-utils.ts
@@ -21,6 +21,11 @@ export function isTriangleCCW(a: Vector, b: Vector, c: Vector) {
   // return (b.x - a.x) * (c.y - a.y) > (b.y - a.y) * (c.x - a.x);
 }
 
+export function isTriangleCW(a: Vector, b: Vector, c: Vector) {
+  const signedArea = (a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x);
+  return signedArea <= 0;
+}
+
 export function isGeoTriangleCCW(triangle: ITriangle) {
   const vertices = triangle.vertexList.items;
   const a = vertices[0];
