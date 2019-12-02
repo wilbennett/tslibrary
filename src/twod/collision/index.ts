3d392e01a4d3fb792608e3341afb28dce36620f5
Wil Bennett <wilbennettjr@live.com>
2019-12-01 20:58:56 -0500

    Added clipper interface.

--- src/twod/collision/index.ts
+++ src/twod/collision/index.ts
@@ -1,6 +1,7 @@
 export * from "./shape-pair";
 export * from "./contact";
 export * from "./collider";
+export * from "./clipper";
 export * from "./collider-base";
 export * from "./simple-collider";
 export * from "./sat-projection";
