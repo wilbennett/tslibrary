7fa6041221c4604ef88ebb8e4b67deba5f0c0a8b
Wil Bennett <wilbennettjr@live.com>
2019-12-06 02:52:04 -0500

    Added segmentSegmentClosestPoints function.

--- src/twod/utils/utils2d.ts
+++ src/twod/utils/utils2d.ts
@@ -1,6 +1,8 @@
 import { MathEx } from '../../core';
 import { Vector } from '../../vectors';
 
+const { epsilon: EPSILON, clamp } = MathEx;
+
 export class CircleSegmentInfo {
   constructor(segmentCount: number) {
     this.segmentCount = segmentCount;
@@ -96,3 +98,65 @@ export function segmentSqrDistToPoint(start: Vector, end: Vector, point: Vector)
 
   return startToPoint.magSquared - projection * projection / segmentLengthSqr;
 }
+
+//
+// Segment.
+// Christer Ericson - Real Time Collision Detection.
+export function segmentSegmentClosestPoints(p1: Vector, q1: Vector, p2: Vector, q2: Vector, c1: Vector, c2: Vector) {
+  const d1 = q1.subO(p1); // Direction vector of segment S1
+  const d2 = q2.subO(p2); // Direction vector of segment S2
+  const r = p1.subO(p2);
+  const a = d1.dot(d1); // Squared length of segment S1, always nonnegative
+  const e = d2.dot(d2); // Squared length of segment S2, always nonnegative
+  const f = d2.dot(r);
+
+  // Check if either or both segments degenerate into points
+  if (a <= EPSILON && e <= EPSILON) { // Both segments degenerate into points
+    c1.copyFrom(p1);
+    c2.copyFrom(p2);
+    return c1.subO(c2).dot(c1.subO(c2));
+  }
+
+  let s: number;
+  let t: number;
+
+  if (a <= EPSILON) { // First segment degenerates into a point
+    s = 0;
+    t = f / e; // s = 0 => t = (b*s + f) / e = f / e
+    t = clamp(t, 0, 1);
+  } else {
+    const c = d1.dot(r);
+
+    if (e <= EPSILON) { // Second segment degenerates into a point
+      t = 0;
+      s = clamp(-c / a, 0, 1); // t = 0 => s = (b*t - c) / a = -c / a
+    } else { // The general nondegenerate case starts here
+      const b = d1.dot(d2);
+      const denom = a * e - b * b; // Always nonnegative
+      // If segments not parallel, compute closest point on L1 to L2 and
+      // clamp to segment S1. Else pick arbitrary s (here 0)
+      if (denom !== 0) {
+        s = clamp((b * f - c * e) / denom, 0, 1);
+      } else
+        s = 0;
+      // Compute point on L2 closest to S1(s) using
+      // t = Dot((P1 + D1*s) - P2,D2) / Dot(D2,D2) = (b*s + f) / e
+      t = (b * s + f) / e;
+      // If t in [0,1] done. Else clamp t, recompute s for the new value
+      // of t using s = Dot((P2 + D2*t) - P1,D1) / Dot(D1,D1)= (t*b - c) / a
+      // and clamp s to [0, 1]
+      if (t < 0) {
+        t = 0;
+        s = clamp(-c / a, 0, 1);
+      } else if (t > 1) {
+        t = 1;
+        s = clamp((b - c) / a, 0, 1);
+      }
+    }
+  }
+
+  p1.displaceByScaledO(d1, s, c1);
+  p2.displaceByScaledO(d2, t, c2);
+  return c1.subO(c2).dot(c1.subO(c2));
+}
+
