f464c4d9babb7e83a540881bcb3acbdb4dda826b
Wil Bennett <wilbennettjr@live.com>
2019-12-07 19:49:01 -0500

    Freeze empty mass info.

--- src/core/mass-info.ts
+++ src/core/mass-info.ts
@@ -1,15 +1,15 @@
 export class MassInfo {
-    constructor(mass: number, inertia: number) {
-        this.mass = mass;
-        this.inertia = inertia;
-        this.massInverse = mass !== 0 ? 1 / mass : 0;
-        this.inertiaInverse = inertia !== 0 ? 1 / inertia : 0;
-    }
+  constructor(mass: number, inertia: number) {
+    this.mass = mass;
+    this.inertia = inertia;
+    this.massInverse = mass !== 0 ? 1 / mass : 0;
+    this.inertiaInverse = inertia !== 0 ? 1 / inertia : 0;
+  }
 
-    readonly mass: number;
-    readonly massInverse: number;
-    readonly inertia: number;
-    readonly inertiaInverse: number;
+  readonly mass: number;
+  readonly massInverse: number;
+  readonly inertia: number;
+  readonly inertiaInverse: number;
 
-    static readonly empty = new MassInfo(0, 0);
+  static readonly empty = Object.freeze(new MassInfo(0, 0));
 }
