769dd272a46962affbb821281f4a437f31eeb5ca
Wil Bennett <wilbennettjr@live.com>
2019-12-11 09:23:55 -0500

    Temporary material attributes calculation.

--- src/twod/collision/shape-pair.ts
+++ src/twod/collision/shape-pair.ts
@@ -7,11 +7,15 @@ export class ShapePair {
   constructor(public readonly shapeA: Shape, public readonly shapeB: Shape) {
     this.inverseMass = shapeA.massInfo.massInverse + shapeB.massInfo.massInverse;
     this.inverseInertia = shapeA.massInfo.inertiaInverse + shapeB.massInfo.inertiaInverse;
-    this.restitution = (shapeA.material.restitution + shapeB.material.restitution) * 0.5;
-    this.staticFriction = (shapeA.material.staticFriction + shapeB.material.staticFriction) * 0.5;
-    this.kineticFriction = (shapeA.material.kineticFriction + shapeB.material.kineticFriction) * 0.5;
+    // this.restitution = (shapeA.material.restitution + shapeB.material.restitution) * 0.5;
+    // this.staticFriction = (shapeA.material.staticFriction + shapeB.material.staticFriction) * 0.5;
+    // this.kineticFriction = (shapeA.material.kineticFriction + shapeB.material.kineticFriction) * 0.5;
     this.contact = new Contact(this);
     this.customData = {};
+
+    this.restitution = Math.min(shapeA.material.restitution, shapeB.material.restitution);
+    this.staticFriction = Math.sqrt(shapeA.material.staticFriction * shapeB.material.staticFriction);
+    this.kineticFriction = Math.sqrt(shapeA.material.kineticFriction * shapeB.material.kineticFriction);
   }
 
   inverseMass: number;
