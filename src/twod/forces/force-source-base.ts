51b0294ff1111228dba62b4814bb11d248b9fae1
Wil Bennett <wilbennettjr@live.com>
2019-12-26 21:42:11 -0500

    Added duration properties and methods.

--- src/twod/forces/force-source-base.ts
+++ src/twod/forces/force-source-base.ts
@@ -1,11 +1,23 @@
 import { ForceSource } from '.';
+import { IWorld } from '..';
 import { Vector } from '../../vectors';
 import { Shape } from '../shapes';
 
 export abstract class ForceSourceBase implements ForceSource {
+  startTime: number = 0;
+  endTime: number = Infinity;
+
+  // @ts-ignore - unused param.
+  initialize(world: IWorld) { }
+  isActive(time: number) { return time >= this.startTime && time <= this.endTime; }
+  isExpired(time: number) { return time > this.endTime; }
+
   protected abstract processCore(shape: Shape, now: number, position: Vector, velocity: Vector): void;
 
   process(shape: Shape, now: number, position: Vector, velocity: Vector) {
+    if (!this.isActive(now)) return;
+    if (this.isExpired(now)) return;
+
     this.processCore(shape, now, position, velocity);
   }
 }
--- src/twod/forces/force-source.ts
+++ src/twod/forces/force-source.ts
@@ -1,7 +1,14 @@
+import { IWorld } from '..';
 import { Vector } from '../../vectors';
 import { Shape } from '../shapes';
 
 
 export interface ForceSource {
+  startTime: number;
+  endTime: number;
+
+  initialize(world: IWorld): void;
+  isActive(time: number): boolean;
+  isExpired(time: number): boolean;
   process(shape: Shape, now: number, position: Vector, velocity: Vector): void;
 }
