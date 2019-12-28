13a4db36fc5e3f477593ea77b9b4c3c1f4be7047
Wil Bennett <wilbennettjr@live.com>
2019-12-28 13:54:57 -0500

    Removed positional correction.

--- src/twod/collision/projection-resolver.ts
+++ src/twod/collision/projection-resolver.ts
@@ -20,15 +20,14 @@ export class ProjectionResolver extends CollisionResolverBase {
 
   resolve(contact: Contact, isLastIteration: boolean) {
     const { shapeA, shapeB } = contact;
-    const invMassA = shapeA.massInfo.massInverse;
-    const invMassB = shapeB.massInfo.massInverse;
-    const contactPoint = contact.points[0]; // Contact keeps deepest point first.
+    // const invMassA = shapeA.massInfo.massInverse;
+    // const invMassB = shapeB.massInfo.massInverse;
+    // const contactPoint = contact.points[0]; // Contact keeps deepest point first.
 
     // if (invMassA === 0 && invMassB === 0) return; //* Immovable pairs screened out in pair manager.
     // if (contactPoint.depth <= 0) return; //* Screened out by narrow phase.
 
     const normal = contact.normalAB;
-    !this.globalPositionalCorrection && this.correctPositions(shapeA, shapeB, invMassA, invMassB, contactPoint.depth, normal);
 
     if (!isLastIteration) return;
 
