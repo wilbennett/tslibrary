5b18b1ad93ff92e364f6001f60ec7e77bd137f08
Wil Bennett <wilbennettjr@live.com>
2020-01-02 11:39:36 -0500

    Fixed proportion bug.

--- src/twod/forces/steering/separate.ts
+++ src/twod/forces/steering/separate.ts
@@ -7,10 +7,12 @@ const difference = dir(0, 0);
 
 export class Separate extends GroupAction {
   minDistance: number = 3;
+  isProportional = true;
 
   protected calcDesiredVelocity(params: ForceProcessParams) {
     const { position } = params;
 
+    const isProportional = this.isProportional;
     const minDistanceSquared = this.minDistance * this.minDistance;
     desired.withXY(0, 0);
     let count = 0;
@@ -21,7 +23,7 @@ export class Separate extends GroupAction {
 
       if (distanceSquared === 0 || distanceSquared >= minDistanceSquared) continue;
 
-      difference.normalizeScale(Math.sqrt(distanceSquared));
+      isProportional && difference.normalizeScale(1 / difference.mag);
       desired.add(difference);
       count++;
     }
